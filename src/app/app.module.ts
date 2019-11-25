71564d054cc250d7fb64aa924972b1503b71ca8f
Sebastia Ramon <sebastiaramon_1994@hotmail.com>
2019-11-25 12:08:39 +0100

    generated components: "login" and "user-info"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { LoginComponent } from './login/login.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { CheckoutComponent } from './checkout/checkout.component';
     ProductsListComponent,
     ProductDetailComponent,
     ShoppingCartComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    LoginComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
