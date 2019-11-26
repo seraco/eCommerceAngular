00a916790a2a9f0aeef410ee80b3f2f3c5f7ec27
Sebastia Ramon <sebastiaramon_1994@hotmail.com>
2019-11-26 15:28:51 +0100

    products list implemented

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,6 +1,7 @@
 export interface Product {
   description: string;
   id: number;
+  image_url: string;
   name: string;
   number_items: number;
   price: number;
--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductsService } from '../products.service';
+import { Product } from '../product';
+
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit() {
+    this.productsService.getProducts().subscribe(
+      products => this.products = products,
+    )
   }
 
+  handleProductClick(product: Product) {
+    console.log(product)
+  }
 }
